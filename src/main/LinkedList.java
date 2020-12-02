8862bd4670773812e6f06e86a1b184532519bcdd
Jiwoo Choi <pelomon2@gmail.com>
2020-12-02 20:53:12 +0900

    remove,iterator, indexof ,contains 추가 + 테스트코드

--- src/main/LinkedList.java
+++ src/main/LinkedList.java
@@ -2,7 +2,8 @@ package main;
 
 /** TODO : toString, iteratable 구현할것 subscriptor 구현할것, null 대응할것. */
 
-import java.util.Objects;
+import java.util.*;
+import java.util.function.Consumer;
 
 /** TODO :
  * nullable checd?
@@ -11,18 +12,59 @@ import java.util.Objects;
  */
 public class LinkedList implements LinkedListADT {
 
+    /**
+     * 이터레이터 사용을 위해 만든 이터레이터 클래스.
+     */
+
+    private class Itr implements Iterator<Integer> {
+
+        private int cursor = 0;
+
+        @Override
+        public boolean hasNext() {
+            return cursor != getSize();
+        }
+
+        @Override
+        public Integer next() {
+            try {
+                int i = cursor;
+                Integer next = get(i);
+                cursor = i + 1;
+                return next;
+            } catch (IndexOutOfBoundsException e) {
+                throw new NoSuchElementException();
+            }
+        }
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Itr();
+    }
+
+    @Override
+    public void forEach(Consumer<? super Integer> action) {
+//        Objects.requireNonNull(action);
+        for (Integer val : this) {
+            action.accept(val);
+        }
+    }
+
+    @Override
+    public Spliterator<Integer> spliterator() {
+        return null;
+    }
+
     private class Node {
         private Integer value;
         private Node next;
+        private Node prev;
 
         public Node(Integer value) {
             this.value = value;
         }
 
-        public Node(Integer value, Node next) {
-            this.value = value;
-            this.next = next;
-        }
     }
 
     private Node head;
@@ -44,6 +86,7 @@ public class LinkedList implements LinkedListADT {
             tail = node;
         } else {
             // add last의 동작을 기본으로 합니다.
+            node.prev = tail;
             tail.next = node;
             tail = node;
         }
@@ -61,12 +104,17 @@ public class LinkedList implements LinkedListADT {
         for (int i = 0 ; i < index ; i++) {
             tempNode = tempNode.next;
         }
+
         node.next = tempNode;
+
         if (index == 0) {
             head = node;
         }
+
         if (size == 0) {
             tail = node;
+        } else {
+            tempNode.prev = node;
         }
         size++;
     }
@@ -88,10 +136,12 @@ public class LinkedList implements LinkedListADT {
     }
 
     public Integer getFirst() {
+        if (size == 0) throw new NoSuchElementException();
         return this.head.value;
     }
 
     public Integer getLast() {
+        if (size == 0) throw new NoSuchElementException();
         return this.tail.value;
     }
 
@@ -105,19 +155,111 @@ public class LinkedList implements LinkedListADT {
 
     @Override
     public boolean contains(Integer value) {
+        return indexOf(value) != -1;
+    }
+
+    @Override
+    public int indexOf(Integer value) {
         if (size == 0) {
-            return false;
+            return -1;
         } else {
+            int cursor = 0;
             Node curr = head;
             do {
                 boolean ret = Objects.equals(value, curr.value);
-                if (ret) return true;
+                if (ret) return cursor;
+                cursor++;
             } while((curr = curr.next) != null);
         }
-        return false;
+        return -1;
     }
 
-    
+    @Override
+    public int lastIndexOf(Integer value) {
+        if (size == 0) {
+            return -1;
+        } else {
+            int cursor = size - 1;
+            Node curr = tail;
+            do {
+                boolean ret = Objects.equals(value, curr.value);
+                if (ret) return cursor;
+                cursor++;
+            } while((curr = curr.next) != null);
+        }
+        return -1;
+    }
+
+    @Override
+    public Integer remove() {
+        if (size == 0) {
+            throw new NoSuchElementException();
+        } else if ( size == 1) {
+            Integer ret = tail.value;
+            tail = null;
+            head = null;
+            this.size = 0;
+            return ret;
+        } else {
+            Integer ret = tail.value;
+            tail = tail.prev;
+            tail.next = null;
+            this.size--;
+            return ret;
+        }
+    }
+
+    // 노드정보얻는 로직 추가.
+    private Node getNodeAt(int index) {
+        if (size == 0) {
+            throw new NoSuchElementException();
+        } else if ( index < 0 || index >= getSize()) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            int cursor = 0;
+            Node curr = head;
+            for (int i = 0 ; i < index; i++) {
+                curr = curr.next;
+            }
+            return curr;
+        }
+    }
+    // s노드정보얻기.
+    @Override
+    public Integer remove(int index) {
+        Node node = getNodeAt(index);
+        // 앞쪽 노드 제거.
+        if (size == 1) {
+            clear();
+        } else {
+            if (node.prev == null) {
+                node.next.prev = null;
+                head = node.next;
+            } else {
+                node.prev.next = node;
+                node.prev.next = node.next;
+            }
+        }
+        return node.value;
+    }
+
+    @Override
+    public boolean remove(Integer value) {
+        int index = indexOf(value);
+        if (index == -1) {
+            return false;
+        } else {
+            remove(index);
+            return true;
+        }
+    }
+
+    @Override
+    public Integer removeFirst() {
+        return remove(0);
+    }
+
+
     //https://opentutorials.org/module/1335/8857
 
 
--- src/test/LinkedListTest.java
+++ src/test/LinkedListTest.java
@@ -133,6 +133,36 @@ class LinkedListTest {
     }
 
     @Test
+    void getLast_functionalTest() {
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(1);
+        assertEquals(linkedList.getLast(), 1);
+    }
+
+    @Test
+    void getLast_from_multiple_functionalTest() {
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add(-15);
+        linkedList.add(25);
+        assertEquals(linkedList.getLast(), 25);
+    }
+
+    @Test
+    void getLast_from_empty_functionalTest() {
+        LinkedList linkedList = new LinkedList();
+        try {
+            linkedList.getLast();
+        } catch (NoSuchElementException e) {
+            // success
+        } catch (Exception e) {
+            fail("Expected : NoSuchElementException!");
+        }
+    }
+
+    @Test
     void clear_FunctionTest() {
         LinkedList linkedList = new LinkedList();
         linkedList.add(15);
@@ -171,7 +201,170 @@ class LinkedListTest {
         assertFalse(linkedList.contains(15));
     }
 
+    @Test
+    void remove_from_empty_FunctionalTest() {
+        LinkedList linkedList = new LinkedList();
+        try {
+            linkedList.remove();
+            fail("Expected : NoSuchElementException");
+        } catch (NoSuchElementException e) {
+        } catch (Exception e) {
+            fail("Expected : NoSuchElementException");
+        }
+    }
 
+    @Test
+    void remove_FunctionalTest(){
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(15);
+        linkedList.remove();
+        assertEquals(0, linkedList.getSize());
+        try {
+            linkedList.getFirst();
+            fail("Expected : NoSuchElementException");
+        } catch (NoSuchElementException e) {
+        } catch (Exception e) {
+            fail("Expected : NoSuchElementException");
+        }
+    }
+
+
+    @Test
+    void remove_from_multiple_FunctionalTest(){
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(15);
+        linkedList.add(20);
+        linkedList.add(25);
+        linkedList.add(30);
+        linkedList.remove();
+        assertEquals(3, linkedList.getSize());
+        assertEquals(25,linkedList.getLast());
+        assertEquals(15, linkedList.getFirst());
+    }
+
+    @Test
+    void remove_idx_FunctionalTest(){
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(15);
+        linkedList.remove(0);
+        assertEquals(0, linkedList.getSize());
+        try {
+            linkedList.getFirst();
+            fail("Expected : NoSuchElementException");
+        } catch (NoSuchElementException e) {
+        } catch (Exception e) {
+            fail("Expected : NoSuchElementException");
+        }
+    }
+
+    @Test
+    void remove_idx_multiple_FunctionalTest(){
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(15);
+        linkedList.add(20);
+        linkedList.add(25);
+        linkedList.add(30);
+        linkedList.remove(2);
+        assertEquals(30, linkedList.get(2));
+        assertEquals(15, linkedList.getFirst());
+    }
+
+
+    @Test
+    void remove_value_FunctionalTest(){
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(15);
+        boolean ok = linkedList.remove(Integer.valueOf(15));
+        assertTrue(ok);
+        try {
+            linkedList.getFirst();
+            fail("Expected : NoSuchElementException");
+        } catch (NoSuchElementException e) {
+        } catch (Exception e) {
+            fail("Expected : NoSuchElementException");
+        }
+    }
+
+    @Test
+    void remove_value_failure_FunctionalTest(){
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(15);
+        boolean ok = linkedList.remove(Integer.valueOf(20));
+        assertFalse(ok);
+    }
+
+    @Test
+    void remove_value_multiple_FunctionalTest(){
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(15);
+        linkedList.add(20);
+        linkedList.add(25);
+        linkedList.add(30);
+        boolean ok = linkedList.remove(Integer.valueOf(20));
+        assertTrue(ok);
+        assertEquals(15, linkedList.getFirst());
+        assertEquals(30, linkedList.getLast());
+    }
+
+
+    @Test
+    void remove_first_FunctionalTest(){
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(15);
+        Integer val = linkedList.removeFirst();
+        assertEquals(val, 15);
+        try {
+            linkedList.getFirst();
+            fail("Expected : NoSuchElementException");
+        } catch (NoSuchElementException e) {
+        } catch (Exception e) {
+            fail("Expected : NoSuchElementException");
+        }
+    }
+
+    @Test
+    void remove_first_multiple_FunctionalTest(){
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(15);
+        linkedList.add(20);
+        linkedList.add(25);
+        linkedList.add(30);
+        assertEquals(15, linkedList.removeFirst());
+        assertEquals(20, linkedList.getFirst());
+    }
+
+    @Test
+    void iterator_functional_test(){
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(15);
+        linkedList.add(20);
+        linkedList.add(25);
+        linkedList.add(30);
+//        List<Integer> expected = Arrays.asList(15,20,25,30);
+//        List<Integer> actual = new ArrayList<Integer>();
+//
+//        for (Integer integer : linkedList) {
+//            actual.add(integer);
+//        }
+//        assert(expected, actual);
+    }
+
+    /**
+     * test hasNext on an empty collection (returns false)
+     * test next() on an empty collection (throws exception)
+     * test hasNext on a collection with one item (returns true, several times)
+     * test hasNext/next on a collection with one item: hasNext returns true, next returns the item, hasNext returns false, twice
+     * test remove on that collection: check size is 0 after
+     * test remove again: exception
+     * final test with a collection with several items, make sure the iterator goes through each item, in the correct order (if there is one)
+     * remove all elements from the collection: collection is now empty
+     */
+    // 헤드가 바뀌는지 항상 테스트할것.
+
+
+
+    //clone
+    //indexof test.
 
 
     /// input이 어떻게 들어올지 예상.
